fb61ecd9f36ce197a8b3b4b3c6dd2e11b06108d9
Sheik Hassan <sheik_hassan@comcast.com>
2018-11-19 11:50:59 -0500

    Initial commit

---
+++ cleanup.py
@@ -0,0 +1,47 @@
+import os
+import re
+import subprocess
+import shutil
+
+
+def clean(initial_directory, shortened_files_directory, wav_files_directory, reprocessed_files, results_json_directory):
+#------- Deletes All Files in Start & Cut_WAV_Dir ----------------------------------
+    # deletedfiles = subprocess.check_output(["rm", "-r", initial_directory + "/*"])
+    for i in os.listdir(initial_directory):
+        os.remove(initial_directory + "/"+ i)
+
+    # deletedfilesindir2 = subprocess.check_output(["rm", "-r", shortened_files_directory + "/*"])
+    for i in os.listdir(shortened_files_directory):
+        os.remove(shortened_files_directory + "/" + i)
+
+    for i in os.listdir(wav_files_directory):
+        os.remove(wav_files_directory + "/" + i)
+
+
+    for i in os.listdir(wav_files_directory):
+        os.remove(wav_files_directory + "/" + i)
+
+    for i in os.listdir(reprocessed_files):
+        os.remove(reprocessed_files + "/" + i)
+
+    for i in os.listdir(results_json_directory):
+        os.remove(reprocessed_files + "/" + i)
+
+def remove_all_files_in_directory(directory):
+    if not os.path.isdir(directory):
+        print("Directory does not exist")
+        return
+
+    for i in os.listdir(directory):
+        os.remove(directory + "/"+ i)
+
+
+def copy_file(src, dest):
+    if not os.path.isfile(src):
+        print("file does not exist")
+        return
+
+    shutil.copy2(src, dest)
+
+# ------- Deletes All Output.wav files
+#     deletedfilesindir2 = subprocess.check_output(["rm", "-r", "*.wav"])
